badfb0ff50cfc6892cba472010dab78c563ed689
Hanfei Shen <qqshfox@gmail.com>
2012-03-04 20:25:15 +0800

    compatibility for osx & gentoo

--- configs/ls.zsh
+++ configs/ls.zsh
@@ -1,1 +1,8 @@
-alias ls='gls --color=auto'
+case $OSTYPE in
+    linux*)
+        alias ls='ls --color=auto'
+        ;;
+    darwin* | freebsd*)
+        export CLICOLOR=1 LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
+        ;;
+esac
--- configs/vim.zsh
+++ configs/vim.zsh
@@ -1,3 +1,8 @@
-export EDITOR="mvim -v"
-alias vi="mvim -v"
-alias vim="mvim -v"
+if command -v mvim &>/dev/null; then
+    export EDITOR="mvim -v"
+    alias vi="mvim -v"
+    alias vim="mvim -v"
+    alias vimdiff="mvim -vd"
+else
+    export EDITOR="vim"
+fi
