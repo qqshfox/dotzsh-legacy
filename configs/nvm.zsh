d9f191ebc8c1090743ebe0cb62e27a5f916df523
Hanfei Shen <hanfei.shen@alo7.com>
2017-04-21 13:51:50 +0800

    improve nvm config

--- configs/nvm.zsh
+++ configs/nvm.zsh
@@ -1,3 +1,11 @@
-NVM_DIR=/Users/hanfei/.nvm
-[[ -s $NVM_DIR/nvm.sh ]] && . $NVM_DIR/nvm.sh # This loads NVM
-[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
+[ -r "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
